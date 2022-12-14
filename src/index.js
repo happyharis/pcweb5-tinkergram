bff929ee346e869b2b9e011575732bfc1259f308
Haris Samingan <muhamad_haris@hotmail.com>
2022-12-15 07:49:27 +0800

    Add error page

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
