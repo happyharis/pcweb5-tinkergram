b811747d726569d90f876ff3e226dc95af9cd84c
Haris Samingan <muhamad_haris@hotmail.com>
2022-12-15 07:52:51 +0800

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Navbar } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar variant="light" bg="light">
+      <Container>
+        <Navbar.Brand href="#">Tinkergram</Navbar.Brand>
+      </Container>
+    </Navbar>
   );
 }
 
